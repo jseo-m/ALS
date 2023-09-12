d4d99439a227817c23f9ee6e561a8d92e0629df5
jseo-m <mon6916@naver.com>
2023-09-13 01:23:14 +0900

    input 작업중

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,32 +7,34 @@ import { useState } from "react";
 export const InputVer1_Mui = ({required, label, placeholder, defaultValue, inputType, hasButton, onChange, ...rest}) =>{
   const [inputHide, setInputHide] = useState(true)
   const [selectDefault, setSelectDefault] = useState(!!rest.selects ? rest.selects[0].value == "" ? rest.selects[0].name : rest.selects[0].value : "");
-  const [selectValue, setSelectValue] = useState(null)
 
-  const onAfterSelect = (e) => {
-    e.target.name = e.target.name.replace("afterSelect","")
-    setSelectDefault(e.target.value)
-    if(e.target.value ==="직접입력"){
-      setSelectValue("")
-    }else{
-      setSelectValue(e.target.value)
-    }
+  const [value, setValue] = useState("")
+  const [beforeSelectValue, setBeforeSelectValue] = useState(selectDefault)
+  const [afterSelectValue, setAfterSelectValue] = useState("")
+
+  const onChangeBeforeInput = ({target}) => {
+    setSelectDefault(target.value)
+    setBeforeSelectValue(target.value)
+    const combineValue =`${target.value}${value}`
+    _onChange({field:target.name, value:combineValue}, onChange)
   }
 
-  const setValue = ({target}) => {
-    const element = document.querySelectorAll(`[name="${target.name}"]`)
-    let value = '';
-    element.forEach((element) => {
-       // MUI 특성상 Select도 Input으로 치환하며, target을 가져왔을시 name, value밖에없음. 일반 input과 구분시켜주기 위한 조건
-      if(element.classList.contains('MuiSelect-nativeInput') || (element.classList.contains('MuiSelect-nativeInput') && !target.tagName)){
-        setSelectDefault(target.value)
-        value += target.value
-      }else{
-        value += element.value;
-      }
-    });
-    console.log(value)
-    onChange({field: target.name, value: value})
+  const onChangeInput = ({target}) => {
+    setValue(target.value)
+    const beforeValue = inputType === "beforeSelect" ? beforeSelectValue : ""
+    const inputValue = target.value
+    const afterValue = inputType === "afterSelect" ? afterSelectValue : ""
+
+    const combineValue = beforeValue + inputValue + afterValue
+    _onChange({field:target.name, value:combineValue}, onChange)
+  }
+
+  const onChangeAfterInput = ({target}) => {
+    setSelectDefault(target.value)
+    const selectValue = target.value==="직접입력" ? "" : target.value
+    setAfterSelectValue(selectValue)
+    const combineValue =`${value}${selectValue}`
+    _onChange({field:target.name, value:combineValue}, onChange)
   }
 
   return(
@@ -48,7 +50,7 @@ export const InputVer1_Mui = ({required, label, placeholder, defaultValue, input
               name={rest.name}
               value={selectDefault}
               label="Age"
-              onChange={setValue}
+              onChange={onChangeBeforeInput}
             >
               {rest.selects.map(select => <MenuItem key={select.value} value={select.value}>{select.name}</MenuItem>)}
             </Select>
@@ -63,7 +65,8 @@ export const InputVer1_Mui = ({required, label, placeholder, defaultValue, input
           required={required} 
           label={inputType === "beforeSelect" ? null : label}
           placeholder={placeholder || label}
-          onChange={setValue}
+          defaultValue={defaultValue || value}
+          onChange={onChangeInput}
           InputLabelProps={{
             shrink: true,
           }}
@@ -84,27 +87,25 @@ export const InputVer1_Mui = ({required, label, placeholder, defaultValue, input
             <TextField
             fullWidth 
             {...rest}
-            type={(inputType === "eyes" && inputHide) ? "password" : "text"}
+            type="text"
+            value={afterSelectValue}
             required={required} 
-            onChange={setValue}
-            value={selectValue}
+            onChange={({target}) => {setAfterSelectValue(target.value),onChange({field:target.name, value:`${value}${target.value}`})}}
             InputLabelProps={{
               shrink: true,
             }}
-            InputProps={{
-              readOnly: !!selectValue
-            }}
           />
           </div>
+          
           <div name="afterSelect">
             <FormControl fullWidth>
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                name={`afterSelect${rest.name}`}
+                name={rest.name}
                 value={selectDefault}
                 label="Age"
-                onChange={onAfterSelect}
+                onChange={onChangeAfterInput}
               >
                 {rest.selects.map(select => <MenuItem key={select.value} value={select.value}>{select.name}</MenuItem>)}
               </Select>
@@ -119,8 +120,75 @@ export const InputVer1_Mui = ({required, label, placeholder, defaultValue, input
         )}
       </section>
       <section>
-        
+        <div id={`message_field_${rest.name}`}></div>
       </section>
     </StyledInputVer1_Mui>
   )
+}
+
+function _onChange(data, onChange){
+  if(onChange) onChange(data)
+
+  _isValid(data)
+}
+
+function _isValid({field, value}){
+
+  const messageElement = document.getElementById(`message_field_${field}`)
+  messageElement.innerHTML = RULE[field]?.guide
+}
+
+export const RULE = {
+  id: {
+    guide: "아이디를 입력해주세요",
+    message: "아이디를 입력해주세요",
+  },
+  email: {
+    regexp: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    guide: "아이디로 사용할 이메일을 입력해주세요",
+    message: "이메일 형식이 아닙니다",
+  },
+  auth_code: {
+    boolean: true,
+    guide: "인증코드가 발송 되었습니다. 이메일로 받은 인증코드를 입력해주세요",
+    pass: "인증되었습니다.",
+    message: "인증코드가 일치하지 않습니다",
+  },
+  password: {
+    regexp: /^.*(?=^.{10,64}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+*=]).*$/,
+    guide: "특수문자, 문자, 숫자 3가지를 모두 포함한 10 ~ 64자리를 입력해주세요",
+    message: "특수문자, 문자, 숫자 3가지를 모두 포함한 10 ~ 64자리를 입력해주세요",
+  },
+  checking_password: {
+    field: "password",
+    guide: "새로운 비밀번호와 동일한 비밀번호를 입력해주세요",
+    pass: "입력하신 비밀번호와 일치합니다.",
+    message: "새로운 비밀번호와 일치하지 않습니다",
+  },
+  name: {
+    guide: "성을 입력해주세요",
+    message: "성을 입력해주세요",
+  },
+  lastName: {
+    guide: "성을 입력해주세요",
+    message: "이름을 입력해주세요",
+  },
+  birthday: {
+    regexp: /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])$/,
+    guide: "생년월일을 선택하거나 달력으로 입력해주세요",
+    message: "생일의 형식은 YYYY-MM-DD 입니다",
+  },
+  country: {
+    guide: "국가를 선택해 주세요",
+    message: "국가를 선택해 주세요",
+  },
+  email_for_reset: {
+    regexp: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    guide: "비밀번호를 초기화할 계정 이메일을 입력해주세요",
+    message: "이메일 형식이 아닙니다",
+  },
+  alias: {
+    optional: true,
+    guide: "등록할 디바이스의 닉네임을 입력하지 않을 경우, 자동 생성되며, 디테일 페이지에서 변경 가능합니다"
+  },
 }
